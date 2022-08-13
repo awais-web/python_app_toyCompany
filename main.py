ad8708512495303f848f9a02905d0fe9b1756914
awais-web <ahmad.webdevelopment@gmail.com>
2022-08-13 16:39:10 +0500

    Comments has been added

--- main.py
+++ main.py
@@ -13,16 +13,18 @@ He sell 1 car in 500Rs
 His other expenses at one car manufacturing is: 18Rs
 '''
 
-
+# to get order quantity
 car = int(input("How many car order you received? : "))
 
 
-
+# to calculate required number of parts with respect to order quantity
 main_body = remote = car
 
 tyres = 4 * car
 side_mirrors = 2 * car
 
+
+# output vaiable
 required_items = f''' Mr. Ahmad you need 
 Car Main Bodies: {main_body}; 
 Side Mirrors: {side_mirrors};
@@ -30,7 +32,7 @@ Tyres: {tyres};
 Remotes: {remote}
 '''
 
-
+# to calculate total costing and profit for this order
 total_cost_1car = 40 +(2*8) + (4*12) + 25
 other_expense = 18
 profit = car * (500 - (18 + total_cost_1car))
